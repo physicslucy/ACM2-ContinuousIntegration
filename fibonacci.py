5d54bac780eb8e25004efb23d65fe39d42994dad
Lucy Upton <lucy@Lucys-MacBook-Air-2.local>
2016-02-22 17:05:43 +0000

    Throw exceptions for negative and fractional values

--- fibonacci.py
+++ fibonacci.py
@@ -2,7 +2,11 @@ def Fibonacci(n):
     """
     Return the n-th value of the Fibonacci sequuence [0, 1, 1, 2, 3, 5, 8, 13, ...]
     """
-    if n<2:
+    if n<0: 
+        raise ValueError("n<0 is not valid")
+    elif round(n) !=n:
+        raise ValueError("Fractional values of n are not allowed")
+    elif n<2:
         return n
     else:
         return Fibonacci(n-1) + Fibonacci(n-2)
