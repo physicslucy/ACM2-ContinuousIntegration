9d9e01c09017e9c88503cd45c6c6eb88ffe9305a
Lucy Upton <lucy@Lucys-MacBook-Air-2.local>
2016-02-22 17:11:03 +0000

    Test that it throws ValueError upon input of negative numbers

--- test_fibonacci.py
+++ test_fibonacci.py
@@ -1,8 +1,17 @@
-def test_fibonacci():
-    """
-    In this function we import file fibonacci.py and test the function inside it, Fibonacci(n) against known values
-    """
+import unittest
 
-    import fibonacci as fb
-    assert fb.Fibonacci(10) == 55
-    assert fb.Fibonacci(5) == 5
+class FibTest(unittest.TestCase):
+
+    def test_fibonacci(self):
+        """
+        In this function we import file fibonacci.py and test the function inside it, Fibonacci(n) against known values
+        """
+
+        import fibonacci as fb
+        assert fb.Fibonacci(10) == 55
+        assert fb.Fibonacci(5) == 5
+
+
+    def test_Fib_throws_exception(self):
+        import fibonacci as fb
+        self.assertRaises(ValueError, fb.Fibonacci, -5)
